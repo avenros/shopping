0ff373e9513db0bc4258a6b3994b617072ceab42
avenros <154999483+avenros@users.noreply.github.com>
2023-12-27 18:18:42 +0000

    render template

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 app.config.from_mapping(
-    SECRET_KEY="dev",
+    SECRET_KEY="f4uIDRBJyZN2vrKtKt6PjNPk7fOYDur",
 )
 app.config.from_prefixed_env()
 
 @app.route("/")
 def index():
-    return "Hello, App!"
+    return render_template("views/index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
